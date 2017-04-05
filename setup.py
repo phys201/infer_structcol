ed63f4eafe373eb8add3940ab667917b36b2b682
victoriahwang89 <vhwang@g.harvard.edu>
2017-04-05 13:08:27 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup
 
+setup(name='infer_structcol',
+      version='0.0.1',
+      description='Inference of parameters of structural color samples',
+      requires=['structural-color','numpy', 'scipy','emcee','matplotlib', 'pint']
+      url='http://github.com/p201-sp2016/infer_structcol',
+      author='Mandrill Rumps',
+      author_email='vhwang@g.harvard.edu',
+      license='GNU GPL v3',
+      packages=['infer_structcol'],
+      zip_safe=False)
