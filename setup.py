7d79bb9a2c82f23181b6ff06491a915e57c1f781
victoriahwang89 <vhwang@g.harvard.edu>
2017-04-14 14:29:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+# To run this package, please git clone or download the structural-color package
+# from https://github.com/manoharan-lab/structural-color into the root directory
+# of your infer-structcol package. 
+
 from setuptools import setup
 
 setup(name='infer_structcol',
       version='0.0.1',
       description='Inference of parameters of structural color samples',
-      requires=['structural-color','numpy', 'scipy','emcee','matplotlib', 'pint']
+      requires=['structural-color','numpy', 'scipy','emcee','matplotlib', 'pint'],
       url='http://github.com/p201-sp2016/infer_structcol',
       author='Mandrill Rumps',
       author_email='vhwang@g.harvard.edu',
