417a9b68ba2018747a4d33cc6fec737b3770fde7
Solomon Barkley <barkley@g.harvard.edu>
2017-04-17 13:52:35 -0400

    update basic framework

--- infer_structcol/model.py
+++ infer_structcol/model.py
@@ -1,4 +1,6 @@
+import multiprocessing as mp
 import numpy as np
+import pandas as pd
 from .main import rescale
 
 def calc_prior(theta, phi_guess):
@@ -15,23 +17,19 @@ def calc_prior(theta, phi_guess):
     var = .0025 # based on expected normal range
     return np.exp(-(vol_frac-phi_guess)**2/var)
 
-def calc_likelihood(spect_theory, spect_meas, sigma_theory, sigma_meas, wavelength, l0, l1):
+def calc_likelihood(data, theory, l0, l1):
     """
     returns likelihood of obtaining an experimental dataset from a given spectrum
     
     Parameters:
-        spect_theory: a spectrum calculated from the MC model (array of length N)
-        spect_meas: dependent variable - experimental measurements (array of length N)
-        sigma_theory: uncertainty associated with the probabilistic MC model (array of length N)
-        sigma_meas: uncertainty assocated with the experimental measurements (array of length N)
-        wavelength: independent variable (array of length N)
+        data: Experimental measurements (Spectrum object)
+        theory: MC calculation results (Spectrum object)
         l0: constant loss parameter (float)
         l1: effect of wavelength on losses (float)
     """
-    
     loss = l0 + l1*rescale(wavelength)
-    residual = (spect_data - (1-loss)*spect_theory)
-    var_eff = sigma_theory**2 + sigma_data**2
+    residual = data.reflectance - (1-loss)*theory.reflectance
+    var_eff = data.sigma_r**2 + theory.sigma_r**2
     chi_square = np.sum(residual**2/var_eff)
     prefactor = 1/np.prod(np.sqrt(2*np.pi*var_eff))
     return prefactor * np.exp(-chi_square/2)
@@ -43,20 +41,31 @@ def log_posterior(theta, data, sample, phi_guess):
     Parameters:
         theta: set of parameters (list-like)
         data: contains observed reflectance and uncertainties (Spectrum object)
-        sample: contains information about the sample that produced data (Sample object) 
+        sample: contains information about the sample that produced data (Sample object)
+        phi_guess: expected volume fraction of the sample
     """
     vol_frac, l0, l1 = theta
-    theory_spectrum = do_some_monte_carlo_calculation(vol_frac, sample, data.wavelength)
+    if not sample.wavelength == data.wavelength:
+        raise ValueError("Sample and data must share the same set of wavelengths")
 
-    likelihood = calc_likelihood(theory_spectrum, data.meas, sigma_theory, data.sigma, data.wavelength, l0, l1)
-    # Not sure where sigma_theory comes from here
+    theory_spectrum = do_some_monte_carlo_calculation(vol_frac, sample)
+                        # returns a spectrum_object & assumes some uncertainty
 
+    likelihood = calc_likelihood(data_spectrum, theory_spectrum, l0, l1)
     return np.log(likelihood) + np.log(calc_prior(theta, phi_guess))
 
-def sample_parameters(data, sample, nwalkers=50, nsteps=500, burn_in_time=0, phi_guess = None):
-
-    if phi_guess is None:
-        phi_guess = 0.55 #This is a reasonable mid-point between dilute suspension and close-packed crystal
+def sample_parameters(data, sample, nwalkers=50, nsteps=500, burn_in_time=0, phi_guess = 0.55):
+    """
+    performs MCMC calculation and outputs DataFrame of parameters and log-probability
+    Parameters:
+        data: contains observed reflectance and uncertainties (Spectrum object)
+        sample: contains information about the sample that produced data (Sample object)
+        nwalkers: number of parallelized walkers to use in MCMC computation
+        nsteps: total number of steps taken by each walker
+        burn_in_time: number of inital steps to be removed from the resturned samples
+        phi_guess: expected volume fraction of the sample (defaults to 0.55),
+            chosen as a reasonable mid-point between dilute suspension and close-packed crystal
+    """
 
     # set expected values to initialize walkers
     expected_vals = np.array([phi_guess,.5,0])
@@ -65,7 +74,10 @@ def sample_parameters(data, sample, nwalkers=50, nsteps=500, burn_in_time=0, phi
     # set walkers in a distribution with width .05
     starting_positions = [expected_vals + 0.05*np.random.randn(ndim)) for i in range(nwalkers)]
     
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior, args=[data, sample, phi_guess])
+    # figure out how many threads to use
+    nthreads = np.min(nwalkers, mp.cpu_count())
+
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior, args=[data, sample, phi_guess], threads=nthreads)
     sampler.run_mcmc(starting_positions, nsteps)
     traces = np.concatenate([sampler.chain[:,burn_in_time:,:], sampler.lnprobability[:,burn_in_time:,np.newaxis]],axis=2).reshape(-1, ndim+1).T
     return pd.DataFrame({key: traces[val] for val, key in enumerate(['vol_frac','l0','l1','lnprob'])})
--- infer_structcol/tests/test_basics.py
+++ infer_structcol/tests/test_basics.py
@@ -1,5 +1,6 @@
 import infer_structcol
 import os
+import numpy as np
 from numpy.testing import assert_equal
 
 def test_structcol_import():
@@ -7,5 +8,8 @@ def test_structcol_import():
     dummy_trajectory = mc.Trajectory("position", "direction", "weight", "nevents")
     assert_equal(dummy_trajectory.position, "position")
 
-
-
+def test_rescale():
+    from infer_structcol.main import rescale
+    inarray = np.array([2,3,12])
+    outarray = np.array([0,.1,1])
+    assert_equal(rescale(inarray),outarray)
