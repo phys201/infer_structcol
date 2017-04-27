5e85cc75902bc0ddad34e6155f47ad6a2f7dd2d3
victoriahwang89 <vhwang@g.harvard.edu>
2017-04-27 15:47:58 -0400

    Delete __init__.py

--- build/lib/infer_structcol/__init__.py
+++
@@ -1,4 +0,0 @@
-from .main import Spectrum, Sample
-from .io import convert_data, load_spectrum
-from .model import log_posterior, get_distribution, run_mcmc
-from .run_structcol import calc_reflectance
