4cd40b338b60d73b683e8ace0bc766429a07b44c
victoriahwang89 <vhwang@g.harvard.edu>
2017-05-01 12:00:19 -0400

    updated run_structcol to account for transmission, fixed typos and added docstring

--- infer_structcol/tests/test_main.py
+++ infer_structcol/tests/test_main.py
@@ -1,14 +1,20 @@
+'''
+This file tests functions from main.py.
+'''
+
 from infer_structcol.main import Spectrum, Sample, extend_array, rescale, find_close_indices, check_wavelength
 import numpy as np
 from numpy.testing import assert_equal, assert_raises
 
 def test_Spectrum():
+    # Test if Spectrum object is created correctly
     spect = Spectrum([300,500],reflectance = [0.5,0.5], sigma_r = [0.1,0.1])
     assert_equal(spect.wavelength, np.array([300,500]))
     spect1 = Spectrum(500,reflectance = 0.5, sigma_r = 0.1)
     assert_equal(spect1.sigma_r, np.array([0.1]))
 
 def test_Sample():
+    # Test if Sample object is created correctly 
     samp = Sample([300,500],0.5, 100, 1.5, 1.3)
     assert_equal(samp.particle_index, np.array([1.5, 1.5]))
 
