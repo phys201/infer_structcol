0a1fe6b5728f8b5e5506163cb0365ed652fdd19f
Solomon Barkley <barkley@g.harvard.edu>
2017-05-03 17:27:58 -0400

    adjust a test to use assert_almsot_equal

--- infer_structcol/tests/test_inference.py
+++ infer_structcol/tests/test_inference.py
@@ -15,7 +15,7 @@ def test_calc_max_like():
     theta = (0.55, 0, 0) #these are the default starting values for lmfit calculation
     spect = calc_model_spect(sample, theta, seed=2)
     max_like_vals = find_max_like(spect, sample, seed=2)
-    assert_equal(max_like_vals, (0.55000000000000004,0.0,0.0,0.0,1.4901161193847656e-08))
+    assert_almost_equal(max_like_vals, (0.55,0.0,0.0,0.0,0))
 
 def test_run_mcmc():
     spectrum = Spectrum(500, reflectance = 0.5, sigma_r = 0.1)
