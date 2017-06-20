a6fa069a2da6835882b9aafcb1de33677150823e
victoriahwang89 <vhwang@g.harvard.edu>
2017-06-20 15:05:08 -0400

    removed some if else statements

--- infer_structcol/inference.py
+++ infer_structcol/inference.py
@@ -15,7 +15,7 @@ theta_range_default = {'min_phi':0.1, 'max_phi':0.74, 'min_radius':10.,
                        'max_radius': 1000., 'min_thickness':1., 'max_thickness':1000., 
                        'min_l0_r':0, 'max_l0_r':1, 'min_l1_r':-1, 'max_l1_r':1,
                        'min_l0_t':0, 'max_l0_t':1, 'min_l1_t':-1, 'max_l1_t':1}  # radius in nm, thickness in um
-
+                       
 # define initial guesses for theta in case the user doesn't give an initial guess. 
 # These values were arbitrarily chosen. It is highly recommended that the user
 # passes a good initial guess based on their knowledge of the system. 
@@ -50,17 +50,15 @@ def find_max_like(data, sample, theta_guess, theta_range, seed=None):
         best fit (phi, radius, thickness, l0, l1) or (phi, radius, thickness, 
         l0_r, l1_r, l0_t, l1_t) as floats
     '''
+    # define a function that takes in a dictionary of the initial theta and 
+    # calculates the residuals to be minimized 
     def resid(params):
-        if 'reflectance' in data.keys() and 'transmittance' in data.keys():
-            theta = (params['phi'], params['radius'],  params['thickness'], 
-                     params['l0_r'], params['l1_r'], params['l0_t'], params['l1_t'])
-        elif 'reflectance' in data.keys():
-            theta = (params['phi'], params['radius'],  params['thickness'], 
-                     params['l0_r'], params['l1_r'])
-        else:
-            theta = (params['phi'], params['radius'],  params['thickness'],
-                     params['l0_t'], params['l1_t'])
-
+        theta = []
+        for key in params:
+            theta.append(params[key])
+        # make the theta into a tuple
+        theta = tuple(theta)
+       
         theory_spect = calc_model_spect(sample, theta, seed)
         resid_spect = calc_resid_spect(data, theory_spect)
 
@@ -105,18 +103,32 @@ def run_mcmc(data, sample, nwalkers, nsteps, theta_guess = theta_guess_default, 
         if key not in theta_range_default:
             raise KeyError('Parameter {0} in theta_range is not defined correctly'.format(str(key)))
     theta_range_default.update(theta_range) 
-    theta_range = theta_range_default
-
+    
     # set initial guesses to initialize walkers. If the user inputs an incorrect key, then raise an Error
     for key in theta_guess:
         if key not in theta_guess_default:
             raise KeyError('Parameter {0} in theta_guess is not defined correctly'.format(str(key)))
     theta_guess_default.update(theta_guess) 
-    theta_guess = theta_guess_default
+    
+    # update the theta_guess and theta_range dictionaries depending on whether
+    # the user inputs reflectance and/or transmittance data
+    guess_keylist = ['phi', 'radius', 'thickness']
+    if 'reflectance' in data.keys():
+        guess_keylist = guess_keylist + ['l0_r', 'l1_r']
+    if 'transmittance' in data.keys():
+        guess_keylist = guess_keylist + ['l0_t', 'l1_t']
+    min_range_keylist = ['min_'+key for key in guess_keylist]
+    max_range_keylist = ['max_'+key for key in guess_keylist]
+    range_keylist = [None]*(len(min_range_keylist) + len(max_range_keylist))
+    range_keylist[::2] = min_range_keylist
+    range_keylist[1::2] = max_range_keylist
+    
+    theta_guess = {key:theta_guess_default[key] for key in guess_keylist}
+    theta_range = {key:theta_range_default[key] for key in range_keylist}
 
     theta = find_max_like(data, sample, theta_guess, theta_range, seed)
     ndim = len(theta)
-      
+
     # set walkers in a distribution with width .05
     vf = np.clip(theta[0]*np.ones(nwalkers) + 0.01*np.random.randn(nwalkers), 
                  theta_range['min_phi'], theta_range['max_phi'])
@@ -124,21 +136,25 @@ def run_mcmc(data, sample, nwalkers, nsteps, theta_guess = theta_guess_default, 
                  theta_range['min_radius'], theta_range['max_radius'])
     thickness = np.clip(theta[2]*np.ones(nwalkers) + 1*np.random.randn(nwalkers), 
                  theta_range['min_thickness'], theta_range['max_thickness'])
+    
+   # Clip the distributions such that there are no walkers out of prior ranges.
+   # The limits for l0_r and l1_r are fixed and the same as for l0_t and l1_t, 
+   # so we can use the default values directly
     l0 = np.clip(theta[3]*np.ones(nwalkers) + 0.01*np.random.randn(nwalkers), 
-                 theta_range['min_l0_r'], theta_range['max_l0_r'])
+                 theta_range_default['min_l0_r'], theta_range_default['max_l0_r'])
     # clip the sum of l0 and l1 and subtract l0 to ensure that the clipped 
     # l1 satisfies 0 < l0 + l1 < 1        
     l1 = np.clip((theta[3] + theta[4]) * np.ones(nwalkers) + 
-                0.01*np.random.randn(nwalkers), theta_range['min_l1_r'], theta_range['max_l0_r']) - l0
-    
+                  0.01*np.random.randn(nwalkers), theta_range_default['min_l1_r'], theta_range_default['max_l1_r']) - l0
+                         
     if ndim == 5:
         theta = np.vstack((vf,radius,thickness,l0,l1)).T.tolist()
-
+            
     if ndim == 7:
         l0_1 = np.clip(theta[5] * np.ones(nwalkers) + 
-                       0.01*np.random.randn(nwalkers), theta_range['min_l0_t'], theta_range['max_l0_t'])
+                       0.01*np.random.randn(nwalkers), theta_range_default['min_l0_r'], theta_range_default['max_l0_r'])
         l1_1 = np.clip((theta[5] + theta[6]) * np.ones(nwalkers) + 
-                       0.01*np.random.randn(nwalkers), theta_range['min_l1_t'], theta_range['max_l1_t']) - l0_1
+                       0.01*np.random.randn(nwalkers), theta_range_default['min_l1_r'], theta_range_default['max_l1_r']) - l0_1
         theta = np.vstack((vf,radius,thickness,l0,l1,l0_1,l1_1)).T.tolist()
 
     # figure out how many threads to use
